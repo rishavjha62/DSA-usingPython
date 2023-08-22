cb9e86ad49e4e0fdaa113278a6b196f94e6d57ed
rishavjha62 <rishavjha62@gmail.com>
2023-08-23 00:59:51 +0530

    adding one last method

---
+++ LinkedList/Linked-List-Basics/reverse-method.py
@@ -0,0 +1,120 @@
+class Node():
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append_list(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else :
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        self.length -= 1
+        if self.length == 0:
+            self.tail == None
+        return temp
+    
+    def get_item(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        
+    def set_value(self, index, value):
+        temp = self.get_item(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0: 
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get_item(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove_item(self, index):       
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0: 
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        prev = self.get_item(index-1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse_item(self):
+        temp = self.head
+        before = None
+        after = temp.next
+        self.head = self.tail
+        self.tail = temp
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+            
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+
+my_linked_list = LinkedList(0)
+my_linked_list.append_list(1)
+my_linked_list.append_list(2)
+my_linked_list.append_list(4)
+
+my_linked_list.print_list()
+print()
+
+my_linked_list.reverse_item()
+my_linked_list.print_list()
+
